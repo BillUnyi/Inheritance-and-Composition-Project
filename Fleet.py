fa8b1700e0c4abf0308fb1ad126031163f05afdb
BillyUnyi <36928559+BillyUnyi@users.noreply.github.com>
2018-03-08 21:16:54 -0600

    Update Fleet.py
    added class Airplane and updated the aircraft carrier design

--- Fleet.py
+++ Fleet.py
@@ -29,22 +29,48 @@ class AircraftCarrier(object):
     """Draws an aircraft carrier and it's squadron"""
     def __init__(self, x, y, color, outline, win):
         self.drawAircraftCarrier(x, y, color, outline, win)
-        airplane1 = Airplane(-100, -100, "Black", "Gray", win)
+        airplane1 = Airplane(x, y, 25, 0, Color.darkGray, "Black", win)
+        airplane2 = Airplane(x, y, 250, -300, Color.darkGray, "Black", win)
+        airplane3 = Airplane(x, y, -200, -400, Color.darkGray, "Black", win)
 
     def drawAircraftCarrier(self, x, y, color, outline, win):
-        ship = Polygon(Point(x, y), Point(x + 350, y), Point(x + 340, y + 100), Point(x + 75, y + 100))
+        ship = Polygon(Point(x, y), Point(x + 75, y - 25), Point(x + 300, y - 20), Point(x + 350, y + 10), Point(x + 340, y + 80), Point(x + 100, y + 100))
         ship.setFill(color)
         ship.setOutline(outline)
         ship.draw(win)
+        shipBase = Polygon(Point(x, y), Point(x + 75, y - 25), Point(x + 300, y - 20), Point(x + 350, y + 10), Point(x + 100, y + 20))
+        shipBase.setFill(color)
+        shipBase.setOutline(outline)
+        shipBase.draw(win)
+        line = Line(Point(x + 100, y + 100), Point(x + 100, y + 20))
+        line.setOutline(outline)
+        line.draw(win)
 
 class Airplane(object):
     """Draws a fighter jet"""
-    def __init__(self, x, y, color, outline, win):
-        self.drawAirplane(x, y, color, outline, win)
+    def __init__(self, X, Y, x, y, color, outline, win):
+        self.drawAirplane(X, Y, x, y, color, outline, win)
 
-    def drawAirplane(self, x, y, color, outline, win):
-        plane = Polygon(Point())
+    def drawAirplane(self, X, Y, x, y, color, outline, win):
+        rightWing = Polygon(Point(X + x + 60, Y + y), Point(X + x + 135, Y + y - 5), Point(X + x + 100, Y + y - 30))
+        rightWing.setFill(color)
+        rightWing.setOutline(outline)
+        rightWing.draw(win)
+        plane = Polygon(Point(X + x, Y + y), Point(X + x + 50, Y + y - 10), Point(X + x + 100, Y + y - 15), Point(X + x + 150, Y + y - 15),
+                        Point(X + x + 155 , Y + y - 10), Point(X + x + 158, Y + y), Point(X + x + 150, Y + y + 5), Point(X + x + 100, Y + y + 5),
+                        Point(X + x + 50, Y + y + 2))
+        plane.setFill(color)
+        plane.setOutline(outline)
+        plane.draw(win)
+        leftWing = Polygon(Point(X + x + 75, Y + y - 2), Point(X + x + 140, Y + y - 5), Point(X + x + 180, Y + y + 15))
+        leftWing.setFill(color)
+        leftWing.setOutline(outline)
+        leftWing.draw(win)
 
+class Color(object):
+    """Holds colors"""
+    lightGray = color_rgb(121, 121, 121)
+    darkGray = color_rgb(80,80,80)
 
 def main():
     win = GraphWin("Fleet", 1900, 1040) #fits my window at home change if nessasary
@@ -54,4 +80,3 @@ def main():
     win.close()
 
 main()
-
