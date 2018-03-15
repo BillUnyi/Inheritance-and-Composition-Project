f0bd93b00c062a62e4ac039d92315953bb8c5235
BillyUnyi <36928559+BillyUnyi@users.noreply.github.com>
2018-03-14 23:23:31 -0500

    Update Fleet.py
    added a day and night cycle called class TimeOfDay, tanks can fire when in war, added class Nuke

--- Fleet.py
+++ Fleet.py
@@ -1,4 +1,4 @@
-from graphics import *
+from Graphics import *
 import random
 
 class Window(object):
@@ -11,11 +11,14 @@ class Water(object):
     """draws the water"""
     def __init__(self, color, win):
         self.water = None
+        self.sun = None
+        self.moon = None
         self.island = None
         self.rect = None
         self.rect1 = None
         self.rect2 = None
         self.line = None
+        self.drawSun(win)
         self.drawWater(color, win)
         self.drawFlag(win)
         self.drawIsland(win)
@@ -52,6 +55,18 @@ class Water(object):
         self.line.setOutline("Black")
         self.line.setWidth(3)
         self.line.draw(win)
+
+    def drawSun(self, win):
+        self.sun = Circle(Point(1700, 150), 50)
+        self.sun.setFill("Yellow")
+        self.sun.setOutline("Black")
+        self.sun.draw(win)
+
+    def drawMoon(self, win):
+        self.moon = Circle(Point(1700, 150), 50)
+        self.moon.setFill(Color.lightGray)
+        self.moon.setOutline(Color.darkGray)
+        self.moon.draw(win)
 
 class Fleet(object):
     """draws everything from the fleet"""
@@ -251,54 +266,71 @@ class SuperBoat(Boat):
 
 class Tank(object):
     """draws a tank"""
+    TANKS = []
     def __init__(self, X, Y, x, y, color, outline, win):
         personnel1 = Personnel(X + x, Y + y, 65, -55, Color.lightTan, Color.darkGray1, "Blue4", "Black", win)
+        self.rect = None
+        self.rect1 = None
+        self.circ = None
+        self.circ1 = None
+        self.line = None
+        self.cannon = None
+        self.turret = None
+        self.body = None
+        self.fire = None
         self.drawTreds(X, Y, x, y, color, outline, win)
         self.drawCannon(X, Y, x, y, color, outline, win)
         self.drawBody(X, Y, x, y, color, outline, win)
+        self.drawFire(X, Y, x, y, win)
+        Tank.TANKS.append(self)
 
     def drawTreds(self, X, Y, x, y, color, outline, win):
-        rect = Rectangle(Point(X + x, Y + y - 5), Point(X + x + 96, Y + y))
-        rect.setFill(color)
-        rect.setOutline(outline)
-        rect.draw(win)
-        rect = Rectangle(Point(X + x, Y + y), Point(X + x + 96, Y + y + 15))
-        rect.setFill("Black")
-        rect.setOutline("Black")
-        rect.draw(win)
-        circ = Circle(Point(X + x, Y + y + 8), 7)
-        circ.setFill("Black")
-        circ.setOutline("Black")
-        circ.draw(win)
-        circ = Circle(Point(X + x + 96, Y + y + 8), 7)
-        circ.setFill("Black")
-        circ.setOutline("Black")
-        circ.draw(win)
+        self.rect = Rectangle(Point(X + x, Y + y - 5), Point(X + x + 96, Y + y))
+        self.rect.setFill(color)
+        self.rect.setOutline(outline)
+        self.rect.draw(win)
+        self.rect1 = Rectangle(Point(X + x, Y + y), Point(X + x + 96, Y + y + 15))
+        self.rect1.setFill("Black")
+        self.rect1.setOutline("Black")
+        self.rect1.draw(win)
+        self.circ = Circle(Point(X + x, Y + y + 8), 7)
+        self.circ.setFill("Black")
+        self.circ.setOutline("Black")
+        self.circ.draw(win)
+        self.circ1 = Circle(Point(X + x + 96, Y + y + 8), 7)
+        self.circ1.setFill("Black")
+        self.circ1.setOutline("Black")
+        self.circ1.draw(win)
         for i in range(9):
-            circ = Circle(Point(X + x + i * 12, Y + y + 8), 6)
-            circ.setFill(outline)
-            circ.setOutline("Black")
-            circ.draw(win)
-        line = Line(Point(X + x, Y + y + 8), Point(X + x + 96, Y + y + 8))
-        line.setOutline(color)
-        line.setWidth(2)
-        line.draw(win)
+            self.circ = Circle(Point(X + x + i * 12, Y + y + 8), 6)
+            self.circ.setFill(outline)
+            self.circ.setOutline("Black")
+            self.circ.draw(win)
+        self.line = Line(Point(X + x, Y + y + 8), Point(X + x + 96, Y + y + 8))
+        self.line.setOutline(color)
+        self.line.setWidth(2)
+        self.line.draw(win)
 
     def drawCannon(self, X, Y, x, y, color, outline, win):
-        cannon = Polygon(Point(X + x + 50, Y + y - 25), Point(X + x + 50, Y + y - 35), Point(X + x - 10, Y + y - 33), Point(X + x - 10, Y + y - 27))
-        cannon.setFill(Color.darkGray)
-        cannon.setOutline(outline)
-        cannon.draw(win)
-        body = Polygon(Point(X + x + 30, Y + y - 15), Point(X + x + 50, Y + y - 40), Point(X + x + 80, Y + y - 40), Point(X + x + 90, Y + y - 15))
-        body.setFill(color)
-        body.setOutline(outline)
-        body.draw(win)
+        self.cannon = Polygon(Point(X + x + 50, Y + y - 25), Point(X + x + 50, Y + y - 35), Point(X + x - 10, Y + y - 33), Point(X + x - 10, Y + y - 27))
+        self.cannon.setFill(Color.darkGray)
+        self.cannon.setOutline(outline)
+        self.cannon.draw(win)
+        self.turret = Polygon(Point(X + x + 30, Y + y - 15), Point(X + x + 50, Y + y - 40), Point(X + x + 80, Y + y - 40), Point(X + x + 90, Y + y - 15))
+        self.turret.setFill(color)
+        self.turret.setOutline(outline)
+        self.turret.draw(win)
 
     def drawBody(self, X, Y, x, y, color, outline, win):
-        body = Polygon(Point(X + x - 10, Y + y - 5), Point(X + x + 106, Y + y - 5), Point(X + x + 96, Y + y - 15), Point(X + x, Y + y - 15), )
-        body.setFill(color)
-        body.setOutline(outline)
-        body.draw(win)
+        self.body = Polygon(Point(X + x - 10, Y + y - 5), Point(X + x + 106, Y + y - 5), Point(X + x + 96, Y + y - 15), Point(X + x, Y + y - 15), )
+        self.body.setFill(color)
+        self.body.setOutline(outline)
+        self.body.draw(win)
+
+    def drawFire(self, X, Y, x, y, win):
+        self.fire = Polygon(Point(X + x - 10, Y + y - 33), Point(X + x - 10, Y + y - 27), Point(X + x - 20, Y + y - 10), Point(X + x - 17, Y + y - 25), Point(X + x - 30, Y + y - 30), Point(X + x - 17, Y + y - 35), Point(X + x - 20, Y + y - 50))
+        self.fire.setFill("Red")
+        self.fire.setOutline("Orange")
 
 class SuperTank(Tank):
     """draws the ultimate tank"""
@@ -315,6 +347,60 @@ class SuperTank(Tank):
         star.setOutline("Black")
         star.draw(win)
 
+class Nuke(object):
+    """drops a nuke"""
+    xValue = 0
+    def __init__(self, win):
+        self.nuke1 = None
+        self.rocket = None
+        self.rect = None
+        self.rect1 = None
+        self.rect2 = None
+        self.rightWing = None
+        self.leftWing = None
+        self.x = Nuke.xValue
+        self.y = -25
+        self.drawNuke(win)
+
+    def drawNuke(self, win):
+        self.nuke1 = Polygon(Point(self.x, self.y), Point(self.x - 15, self.y - 50), Point(self.x - 20, self.y - 100), Point(self.x + 20, self.y - 100), Point(self.x + 15, self.y - 50))
+        self.nuke1.setFill("Red4")
+        self.nuke1.setOutline("Black")
+        self.nuke1.draw(win)
+        self.rocket = Rectangle(Point(self.x - 20, self.y - 300), Point(self.x + 20, self.y - 100))
+        self.rocket.setFill(Color.darkGray)
+        self.rocket.setOutline("Black")
+        self.rocket.draw(win)
+        self.rect = Rectangle(Point(self.x - 15, self.y - 290), Point(self.x - 6, self.y - 110))
+        self.rect.setFill("Red")
+        self.rect.setOutline("Red")
+        self.rect.draw(win)
+        self.rect1 = Rectangle(Point(self.x - 5, self.y - 290), Point(self.x + 4, self.y - 110))
+        self.rect1.setFill("Blue")
+        self.rect1.setOutline("Blue")
+        self.rect1.draw(win)
+        self.rect2 = Rectangle(Point(self.x + 5, self.y - 290), Point(self.x + 15, self.y - 110))
+        self.rect2.setFill("White")
+        self.rect2.setOutline("White")
+        self.rect2.draw(win)
+        self.rightWing = Polygon(Point(self.x - 21, self.y - 300), Point(self.x - 21, self.y - 250), Point(self.x - 35, self.y - 300), Point(self.x - 40, self.y - 350))
+        self.rightWing.setFill("Red")
+        self.rightWing.setOutline("Black")
+        self.rightWing.draw(win)
+        self.leftWing = Polygon(Point(self.x + 21, self.y - 300), Point(self.x + 21, self.y - 250), Point(self.x + 35, self.y - 300), Point(self.x + 40, self.y - 350))
+        self.leftWing.setFill("Red")
+        self.leftWing.setOutline("Black")
+        self.leftWing.draw(win)
+
+    def dropNuke(self):
+        self.nuke1.move(0, 20)
+        self.rocket.move(0, 20)
+        self.rect.move(0, 20)
+        self.rect1.move(0, 20)
+        self.rect2.move(0, 20)
+        self.rightWing.move(0, 20)
+        self.leftWing.move(0, 20)
+        time.sleep(.05)
 
 class Color(object):
     """Holds colors"""
@@ -326,28 +412,82 @@ class Color(object):
     darkTan = color_rgb(97, 48, 0)
     darkBlue = color_rgb(0, 0, 80)
 
-def war(win):
-    while win.checkMouse() == None:
-        win.setBackground(color_rgb(random.randrange(255), random.randrange(255), random.randrange(255)))
-        time.sleep(.05)
+class TimeOfDay(object):
+    """has day, night, and war"""
 
-def day(win, window):
-    win.setBackground(Color.darkBlue)
-    window.water1.water.setFill("Blue4")
-    window.water1.water.setOutline("Blue4")
-    win.getMouse()
+    @staticmethod
+    def war(win, click):
+        if click:
+            click = True
+            Time = 0
+            while Time < 8 and click:
+                win.setBackground(color_rgb(random.randrange(255), random.randrange(255), random.randrange(255)))
+                if random.randrange(5) == 0:
+                    index = random.randrange(len(Tank.TANKS))
+                    Tank.TANKS[index].fire.draw(win)
+                    time.sleep(.1)
+                    Tank.TANKS[index].fire.undraw()
+                coordinate = win.checkMouse()
+                if coordinate is None:
+                    time.sleep(.05)
+                    Time += .05
+                else:
+                    click = False
+                    Nuke.xValue = coordinate.x
+                    nuke = Nuke(win)
+                    for i in range(50):
+                        nuke.dropNuke()
+        return click
+
+    @staticmethod
+    def day(win, window, click):
+        if click:
+            win.setBackground("Cyan")
+            window.water1.moon.undraw()
+            window.water1.drawSun(win)
+            window.water1.water.setFill("Blue")
+            window.water1.water.setOutline("Blue")
+            click = TimeOfDay.clicked(win)
+        return click
+
+    @staticmethod
+    def night(win, window, click):
+        if click:
+            win.setBackground(Color.darkBlue)
+            window.water1.sun.undraw()
+            window.water1.drawMoon(win)
+            window.water1.water.setFill("Blue4")
+            window.water1.water.setOutline("Blue4")
+            click = TimeOfDay.clicked(win)
+        return click
+
+    @staticmethod
+    def clicked(win):
+        click = True
+        Time = 0
+        while Time < 10 and click:
+            coordinate = win.checkMouse()
+            if coordinate is None:
+                time.sleep(.05)
+                Time += .05
+            else:
+                click = False
+                Nuke.xValue = coordinate.x
+                nuke = Nuke(win)
+                for i in range(50):
+                    nuke.dropNuke()
+        return click
 
 def main():
     win = GraphWin("Fleet", 1900, 1040) #fits my window at home, change if nessasary
     win.setBackground("Cyan")
     window = Window(win)
-    win.getMouse()
-    war(win)
-    win.setBackground(Color.darkBlue)
-    window.water1.water.setFill("Blue4")
-    window.water1.water.setOutline("Blue4")
-    win.getMouse()
-    war(win)
+    click = TimeOfDay.clicked(win)
+    while click:
+        click = TimeOfDay.war(win, click)
+        click = TimeOfDay.night(win, window, click)
+        click = TimeOfDay.war(win, click)
+        click = TimeOfDay.day(win, window, click)
     win.close()
 
 main()
